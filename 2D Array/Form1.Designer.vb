73dbe0376d9d41bdf4d7848061560e93e9543555
RedFam888 <Coinoblivion88@gmail.com>
2023-07-10 16:18:07 -0700

    Add project files.

---
+++ 2D Array/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstRecords = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnChk = New System.Windows.Forms.Button()
+        Me.btnAvg = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstRecords
+        '
+        Me.lstRecords.ColumnWidth = 100
+        Me.lstRecords.FormattingEnabled = True
+        Me.lstRecords.ItemHeight = 15
+        Me.lstRecords.Location = New System.Drawing.Point(12, 12)
+        Me.lstRecords.MultiColumn = True
+        Me.lstRecords.Name = "lstRecords"
+        Me.lstRecords.Size = New System.Drawing.Size(294, 289)
+        Me.lstRecords.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(312, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(128, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add Record"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnChk
+        '
+        Me.btnChk.Location = New System.Drawing.Point(312, 79)
+        Me.btnChk.Name = "btnChk"
+        Me.btnChk.Size = New System.Drawing.Size(128, 23)
+        Me.btnChk.TabIndex = 2
+        Me.btnChk.Text = "Check Record"
+        Me.btnChk.UseVisualStyleBackColor = True
+        '
+        'btnAvg
+        '
+        Me.btnAvg.Location = New System.Drawing.Point(312, 146)
+        Me.btnAvg.Name = "btnAvg"
+        Me.btnAvg.Size = New System.Drawing.Size(128, 23)
+        Me.btnAvg.TabIndex = 3
+        Me.btnAvg.Text = "Check Average Time"
+        Me.btnAvg.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(312, 278)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(128, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear Records"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnRandom
+        '
+        Me.btnRandom.Location = New System.Drawing.Point(312, 215)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(128, 23)
+        Me.btnRandom.TabIndex = 5
+        Me.btnRandom.Text = "Random Record"
+        Me.btnRandom.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnRandom)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAvg)
+        Me.Controls.Add(Me.btnChk)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstRecords)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstRecords As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnChk As Button
+    Friend WithEvents btnAvg As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnRandom As Button
+End Class
---
+++ 2D Array/Form1.vb
@@ -0,0 +1,80 @@
+﻿'Program: Lab 1 ---2D Array
+'Purpose: Comp 9 Lab 1
+'Name:Mason Merritt
+'Date: Feb 1, 2023
+
+Option Strict On
+Option Infer Off
+Option Explicit On
+Public Class Form1
+
+    'Array Decleration
+
+    Dim strRecordArray(49, 2) As String
+    Dim intLogicSize As Integer = 0
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        'Input Boxes to Add Record Information
+
+        Dim strName As String = InputBox("Please Enter Your Name:", "Name")
+        Dim strPosition As String = InputBox("Please Enter Staff or Student:", "Position at School")
+        Dim strLaptime As String = InputBox("Please Enter Laptime in Seconds:", "Laptime")
+
+        'Adds Item Name to Listbox
+
+        lstRecords.Items.Add(strName.ToUpper)
+
+        'Add Items to Array
+        strRecordArray(intLogicSize, 0) = strName
+        strRecordArray(intLogicSize, 1) = strPosition
+        strRecordArray(intLogicSize, 2) = strLaptime
+        intLogicSize += 1
+
+    End Sub
+
+    Private Sub btnChk_Click(sender As Object, e As EventArgs) Handles btnChk.Click
+
+        'Check Selected Record
+
+        MessageBox.Show(strRecordArray(lstRecords.SelectedIndex, 0) &
+                        " is a " & strRecordArray(lstRecords.SelectedIndex, 1) &
+                        " they ran a " & strRecordArray(lstRecords.SelectedIndex, 2) & " second laptime")
+
+    End Sub
+
+    Private Sub btnAvg_Click(sender As Object, e As EventArgs) Handles btnAvg.Click
+
+        'Find Average Laptime`
+
+        Dim dblAverage As Double
+        Dim dblLaptime As Double
+
+        For aver As Integer = 0 To intLogicSize
+            dblLaptime += Val(strRecordArray(aver, 2))
+        Next
+        dblAverage = dblLaptime / intLogicSize
+        MessageBox.Show("The average laptime is " & dblAverage.ToString)
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clear Array
+        lstRecords.Items.Clear()
+        Array.Clear(strRecordArray)
+
+    End Sub
+
+    Private Sub btnRandom_Click(sender As Object, e As EventArgs) Handles btnRandom.Click
+
+        'Choose Random Record
+
+        Dim myRandom As New Random
+        Dim i As Integer = lstRecords.Items.Count
+        MessageBox.Show("The computer has chosen " & lstRecords.Items.Item(myRandom.Next(i)).ToString)
+    End Sub
+
+End Class
